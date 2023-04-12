b876ba76452069563561a888c1df20b9c9a5d8b4
Philos Kim <philos99@gmail.com>
2023-04-12 10:59:07 +0900

    bufferline buffer_close_icon configured

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -16,6 +16,8 @@ function M.setup()
       themable = true,
       --diagnostics = "nvim_lsp",
       separator_style = "slant" or "padded_slant",
+      buffer_close_icon = "",
+      close_icon = "",
       show_tab_indicators = true,
       show_buffer_close_icons = true,
       show_close_icon = true,
