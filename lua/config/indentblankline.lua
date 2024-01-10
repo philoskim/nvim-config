029291b68e97c7c087f6d411acc3407a7e26427f
Philos Kim <philos99@gmail.com>
2024-01-10 12:22:18 +0900

    fixed: indent-blankline setup

--- lua/config/indentblankline.lua
+++ lua/config/indentblankline.lua
@@ -1,11 +1,9 @@
 local M = {}
 
 function M.setup()
-  local g = vim.g
-  g.indent_blankline_char = "┊"
-  g.indent_blankline_filetype_exclude = { "help", "packer" }
-  g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-  g.indent_blankline_show_trailing_blankline_indent = false
+  require("ibl").setup {
+    indent = { highlight = highlight, char = "┊" },
+  }
 end
 
 return M
